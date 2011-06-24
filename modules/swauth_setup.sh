782cbdb9bc73f82bb0dff84089008bde1accf264
Marcelo Martins <btorch@gmail.com>
2011-06-24 17:37:37 +0000

    adding a section to install swauth from github if version is 1.4.1

--- modules/swauth_setup.sh
+++ modules/swauth_setup.sh
@@ -18,10 +18,57 @@
 
 swauth_setup (){
 
+    # If swift version is 1.4.1 or greater then swauth needs to be installed from github
+
+    VER_REGEX="^1\.[4-9]\.[0-9]"
+    if [[ "$VERSION" =~ $VER_REGEX ]]; then
+        SWAUTH_TEMP="swauth-src"
+        cd $CURDIR
+
+        printf "\n\t - Starting swauth source installation process \n"
+        if [ ! -d ./$SWAUTH_TEMP ]; then
+            printf "\t\t Creating temporaty swauth source directory \n"
+            mkdir $SWAUTH_TEMP
+        fi
+
+        cd $SWAUTH_TEMP
+        printf "\t\t Downloading swauth source code \n"
+        GIT=`git clone git://github.com/gholt/swauth.git &> /dev/null`
+        cd swauth
+        sleep 2
+        
+        printf "\t\t Building & Installing swauth under /usr/local "
+        python setup.py build 2>&1 >  $CURDIR/git_swauth_build.log
+    
+        CODE=$?
+        if [ $CODE -eq 0 ];then
+            printf "\n\t\t -> Build sucessful "
+        else
+            printf "\t\t\t -> \033[1;31;40m Error found (check log file)  \033[0m\n\n"
+            exit 1
+        fi
+
+        sleep 2
+        python setup.py install 2>&1 >  $CURDIR/git_swauth_install.log
+
+        CODE=$?
+        if [ $CODE -eq 0 ];then
+            printf "\n\t\t -> Install sucessful "
+        else
+            printf "\t\t\t -> \033[1;31;40m Error found (check log file)  \033[0m\n\n"
+            exit 1
+        fi
+
+        printf "\n\n"
+        cd $CURDIR
+        if [ -d $CURDIR/$SWAUTH_TEMP ]; then
+            rm -rf $SWAUTH_TEMP
+        fi
+    fi
+
     SWAUTH_PREP=`which swauth-prep`
     SWAUTH_ADD=`which swauth-add-user`
     SWAUTH_LIST=`which swauth-list`
-
 
     printf "\n\t - SWAuth account prep "
     printf "\n\t\t Setting up swiftops account "
