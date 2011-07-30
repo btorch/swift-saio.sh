1ce156d9a656c241990974fa5be488cf0fd0d45f
Marcelo Martins <btorch@gmail.com>
2011-07-30 17:18:45 +0000

    adding skeleton ... nothing here yet

---
+++ modules/swift_stats_logging_setup.sh
@@ -0,0 +1,18 @@
+#
+#   Author: Marcelo Martins (btorch AT gmail.com)
+#   Created: 2011/07/13
+#
+#   Info:
+#       This a script that is imported (sourced) by the main swift-saio.sh in order 
+#       to setup stats logging for this swift SAIO install.
+#       Created it as a separate file so that it can be re-used or extended wihout 
+#       impacting the main script.
+#
+#       The script still needs to use some variables that are configured in the 
+#       swift-saio.cfg configuration file. 
+#
+
+###########################
+#  STATS LOGGING SETUP
+###########################
+
